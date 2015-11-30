b9cb973650fc56e1615a4aa57764896a6b23db87
Paul Kalauner <p.k@magic-consoles.com>
2015-11-30 17:46:52 +0100

    fixed indentations

--- content.js
+++ content.js
@@ -6,14 +6,19 @@ events.each(function() {
 });
 
 function checkAssignment(event) {
-	var eventlink = event.find("> a");
+  var eventlink = event.find("> a");
   var name = eventlink.text();
   var handler = function(data) {
     var submissionstatussubmitted = $(data).find('.submissionstatussubmitted');
     if (submissionstatussubmitted.length >= 1) {
-			var img = event.find(".icon > img");
-			img.attr("src" , tickpath);
+      var img = event.find(".icon > img");
+      img.attr("src" , tickpath);
     }
   };
   $.get(eventlink.attr('href'), handler);
 }
+events = $(".block_calendar_upcoming .content .event > a");
+
+events.each(function() {
+  checkAssignment($(this));
+});
