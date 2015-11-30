6815d2fb6c987b2f255ba54a4350cc93611e15f1
ReneHollander <rene.hollander@hotmail.de>
2015-11-30 19:25:22 +0100

    fixed some bugs that were introduced with 55a5e41da3ea6174acfe90a45d6481cf534b30d2

--- content.js
+++ content.js
@@ -1,24 +1,17 @@
 var tickpath = chrome.extension.getURL('tick.png');
-events = $(".block_calendar_upcoming .content .event");
-
-events.each(function() {
-  checkAssignment($(this));
-});
 
 function checkAssignment(event) {
   var eventlink = event.find("> a");
   var name = eventlink.text();
-  var handler = function(data) {
+  $.get(eventlink.attr('href'), function(data) {
     var submissionstatussubmitted = $(data).find('.submissionstatussubmitted');
     if (submissionstatussubmitted.length >= 1) {
       var img = event.find(".icon > img");
       img.attr("src" , tickpath);
     }
-  };
-  $.get(eventlink.attr('href'), handler);
+  });
 }
-events = $(".block_calendar_upcoming .content .event > a");
 
-events.each(function() {
+$(".block_calendar_upcoming .content .event").each(function() {
   checkAssignment($(this));
 });
